60bc6f3df0ad4bb65f281688232e5ba2bc594011
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2018-01-05 16:25:36 +0100

    Testing approvals logging

--- src/pkgupdate/main.c
+++ src/pkgupdate/main.c
@@ -64,7 +64,7 @@ const char *hook_preupdate = "/etc/updater/hook_preupdate";
 const char *hook_postupdate = "/etc/updater/hook_postupdate";
 const char *hook_reboot_delayed = "/etc/updater/hook_reboot_required";
 const char *bb_approval_filename = "/etc/tmp/update-state/bb-approvals-file";
-const char *bb_approval_file = NULL;
+FILE *bb_approval_file = NULL;
 
 static bool approved(struct interpreter *interpreter, const char *approval_file, const char **approvals, size_t approval_count) {
 	fputs("testing if it works+\n", bb_approval_file);
