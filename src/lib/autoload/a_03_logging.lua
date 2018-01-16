b3c98178360acb56b2ee48092a3652ea6f9b37c8
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2018-01-16 15:01:24 +0100

    FOX: commands order

--- src/lib/autoload/a_03_logging.lua
+++ src/lib/autoload/a_03_logging.lua
@@ -95,14 +95,15 @@ function restore_cursor()
 end
 
 function show_progress(message, value)
-	set_cursor(row,1)
-	io.write(csi .. "2K")
-	INFO(message)
-	-- ^^ clear previous line
-	save_cursor()
+	-- setup veriables
 	local size = get_screen_size()
 	local row = size[1]
 	local col = size[2]
+	-- clear previous line
+	save_cursor()
+	set_cursor(row,1)
+	io.write(csi .. "2K")
+	INFO(message)
 --	set_cursor(row - 1,1)
 --	io.write(csi .. "2K")
 	set_cursor(row,1)
