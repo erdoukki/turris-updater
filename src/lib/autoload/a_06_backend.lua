0bb32679beefebd1195ceae3c9eabd38dd3499a6
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2018-02-02 10:40:04 +0100

    Wrapper function used in PKG_EXAMINE

--- src/lib/autoload/a_06_backend.lua
+++ src/lib/autoload/a_06_backend.lua
@@ -523,11 +523,11 @@ function pkg_examine(dir)
 	if cidx then
 		for l in cidx:lines() do
 			local fname = l:match("^%s*(/.*%S)%s*")
-			local content, err = utils.slurp(data_dir .. fname)
-			if not content then
-				error(err)
+			if utils.file_exists(data_dir .. fname) then
+				conffiles[fname] = sha256_file(data_dir .. fname)
+			else
+				error("File " .. fname .. " does not exist.")
 			end
-			conffiles[fname] = sha256(content)
 		end
 		cidx:close()
 	end
