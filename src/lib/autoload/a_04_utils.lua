8ea6cb2418652d30e57c0a989534c7b85ef5b27b
Boleslav Březovský <boleslav.brezovsky@nic.cz>
2018-02-02 10:31:15 +0100

    Added FILE_EXISTS func

--- src/lib/autoload/a_04_utils.lua
+++ src/lib/autoload/a_04_utils.lua
@@ -304,4 +304,13 @@ function table_overlay(table)
 	})
 end
 
+--[[
+Check wheter file exists
+]]
+function file_exists(name)
+	local f=io.open(name,"r")
+	if f~=nil then io.close(f) return true else return false end
+ end
+
+
 return _M
